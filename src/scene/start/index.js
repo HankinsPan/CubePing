bba4f0511ee779cf266593d5bc8c2e50f4fdc9e1
HankinsPan <hankins.home@outlook.com>
2018-03-07 23:39:26 +0800

    modify save shotView page

--- src/scene/start/index.js
+++ src/scene/start/index.js
@@ -1,35 +1,110 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import {
     View,
     Text,
     Image,
     ART,
+    Alert,
+    TouchableOpacity,
+    TouchableWithoutFeedback,
+
 } from 'react-native'
+
+import * as color from '../../utils/colors'
+import Canvas from 'react-native-canvas';
+import ViewShot from "react-native-view-shot";
 
 const styles = require('./styles');
 
-class SignPage extends Component{
+class SignPage extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-
+        this.state = {
+            comImgUri: '',
         }
     }
 
+    // componentDidMount() {
+    // this.refs.viewShot.capture().then(uri => {
+    //     console.log("do something with ", uri);
+    //     this.setState({
+    //         comImgUri: uri,
+    //     })
+    //
+    // });
 
-    render(){
+    // console.log("**** canvas ->", canvas.getContext('2d'));
+    // }
 
 
+    // handleCanvas = (canvas) => {
+    //     const ctx = canvas.getContext('2d');
+    //     ctx.fillStyle = 'purple';
+    //     ctx.fillRect(0, 0, 100, 100);
+    // };
 
-        return(
-            <View style={styles.container}>
-                <Text>
-                    hello SignPage
-                </Text>
-            </View>
+    componentWillUnmount() {
+        this.timer && clearTimeout(this.timer);
+    }
+
+    _getView2Img = () => {
+        console.log("_getView2Img ")
+        this.refs.viewShot
+            .capture()
+            .then(uri => {
+                console.log("make shotView uri -> ", uri);
+                this.setState({
+                    comImgUri: uri,
+                })
+            });
+
+        this.timer = setTimeout(() => {
+            Alert.alert(
+                'Long Press Alert',
+                'you can choose shotView to save your album',
+                [
+                    {text: 'Save', onPress: () => this._turnToAlbum()},
+                    {text: 'cancel'}
+                ],
+                {cancelable: false}
+            );
+        }, 500);
+
+    };
+
+    _turnToAlbum = () =>{
+        console.log("_turnToAlbum ")
+    };
+
+
+    render() {
+        console.log('SignPage props ->', this.props);
+
+
+        return (
+            <TouchableWithoutFeedback onLongPress={() => this._getView2Img()}>
+                <View style={styles.container}>
+                    <ViewShot
+                        ref="viewShot"
+                        options={{format: "jpg", quality: 0.9}}>
+
+                        <View style={{width: 120, height: 45, backgroundColor: color.STATE_HEALTH}}/>
+
+
+                    </ViewShot>
+                </View>
+            </TouchableWithoutFeedback>
         )
     }
 }
 
 export default SignPage
+
+
+// <ViewShot
+// ref="viewShot"
+// options={{format: "jpg", quality: 0.9}}
+// style={styles.container}>
+
+// < /ViewShot>
--- src/scene/start/styles.js
+++ src/scene/start/styles.js
@@ -1,15 +1,33 @@
-import {StyleSheet, Platform, Dimensions} from 'react-native';
+import {
+    StyleSheet,
+    Platform,
+    Dimensions
+} from 'react-native';
 
 const window = Dimensions.get('window');
 
 module.exports = StyleSheet.create({
+    originView: {
+        flex: 1,
+    },
+
     container: {
         flex: 1,
         flexDirection: 'column',
-        justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#FDFDFD',
+        justifyContent: 'center',
     },
 
+    imgBgView: {
+        marginTop: 30,
+        width: window.width * 0.75,
+        height: window.width * 0.5,
 
+    },
+
+    shotView: {
+        width: window.width,
+        height: window.width * 2 / 3,
+        backgroundColor: 'rgba(0,0,0,0.05)',
+    },
 });
